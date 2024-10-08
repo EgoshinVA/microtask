5efa67425b7df8795d909676e8ca225b33ec8e81
EgoshinVA <egoshinvit@gmail.com>
2024-10-08 22:50:59 +0700

    useState + filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,43 @@
 import React, {useState} from 'react';
+import {NewComponent} from "./components/newComponent/NewComponent";
+
+export type MoneyType = {
+    banknote: "dollar" | "ruble",
+    nominal: number,
+    number: string
+}
+
+export type FilterType = 'all' | 'dollar' | 'ruble'
+
 
 function App() {
-    let [a, setA] = useState<number>(1)
+    const [money, setMoney] = useState<MoneyType[]>([
+        {banknote: "dollar", nominal: 100, number: "a123456789"},
+        {banknote: "dollar", nominal: 50, number: "b123456789"},
+        {banknote: "ruble", nominal: 100, number: "c123456789"},
+        {banknote: "dollar", nominal: 100, number: "d123456789"},
+        {banknote: "dollar", nominal: 50, number: "e123456789"},
+        {banknote: "ruble", nominal: 100, number: "f123456789"},
+        {banknote: "dollar", nominal: 50, number: "j123456789"},
+        {banknote: "ruble", nominal: 50, number: "h123456789"}
+    ])
 
-    const onClickHandler = () => {
-        setA(++a)
-        console.log(a)
+    const [filter, setFilter] = useState<FilterType>('all')
+
+    let currentMoney = money
+
+    filter === 'ruble' && (currentMoney = money.filter(item => item.banknote === 'ruble'))
+    filter === 'dollar' && (currentMoney = money.filter(item => item.banknote === 'dollar'))
+
+    const setCurrentFilter = (filt: FilterType) => {
+        setFilter(filt)
     }
 
     return (
         <div>
-            {a}
-            <button onClick={onClickHandler}>add</button>
-            <button onClick={()=>{setA(0)}}>0</button>
+            <NewComponent setCurrentFilter={setCurrentFilter} currentMoney={currentMoney}/>
         </div>
+
     );
 }
 
