0eafbace7b638dfedaa7b500a947ec898e35d5c0
EgoshinVA <egoshinvit@gmail.com>
2024-10-08 16:08:50 +0700

    метод map

--- src/NewComponent.tsx
+++ src/NewComponent.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
-import {UserType} from "./App";
+import {CarInfoType} from "./App";
 
 type NewComponentPropsType = {
-    students: UserType[]
+    carInfo: CarInfoType[]
 }
 
-export const NewComponent: React.FC<NewComponentPropsType> = () => {
+export const NewComponent: React.FC<NewComponentPropsType> = (props) => {
     return (
         <div>
-
+            <table>
+                <caption>
+                    New Component
+                </caption>
+                <tr>
+                    <th scope="col">Id</th>
+                    <th scope="col">manufacturer</th>
+                    <th scope="col">model</th>
+                </tr>
+                {props.carInfo.map((car, index)=><tr key={index}>
+                    <th scope="row">{index+1}</th>
+                    <td>{car.manufacturer}</td>
+                    <td>{car.model}</td>
+                </tr>)}
+            </table>
         </div>
     );
 };
